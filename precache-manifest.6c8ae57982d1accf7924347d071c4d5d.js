6ea4200121fd090df189cfed4f2190452d51e56f
krunal-lia <krunallia18@gmail.com>
2019-09-13 14:05:11 +0530

    Updates

---
+++ precache-manifest.6c8ae57982d1accf7924347d071c4d5d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b196afa0deab6c00a5f70a975336705d",
+    "url": "/ExBook/index.html"
+  },
+  {
+    "revision": "bdcac5b1346533f41344",
+    "url": "/ExBook/static/css/main.a22f1838.chunk.css"
+  },
+  {
+    "revision": "61dfab8b06c16f29c4c8",
+    "url": "/ExBook/static/js/2.cc46b07f.chunk.js"
+  },
+  {
+    "revision": "bdcac5b1346533f41344",
+    "url": "/ExBook/static/js/main.d84ee35e.chunk.js"
+  },
+  {
+    "revision": "09006aebc06624212aab",
+    "url": "/ExBook/static/js/runtime~main.c23d08bd.js"
+  },
+  {
+    "revision": "6c50ffa2ade5c8355f29d560cfd60ca2",
+    "url": "/ExBook/static/media/book1.6c50ffa2.jpg"
+  },
+  {
+    "revision": "e6fe85875d964eee1635c5bd4d556925",
+    "url": "/ExBook/static/media/book2.e6fe8587.jpg"
+  },
+  {
+    "revision": "56c7770912a9a008bf3a44c4af1cb555",
+    "url": "/ExBook/static/media/book3.56c77709.jpg"
+  },
+  {
+    "revision": "ecceada92dcf1733280a71348cc64ff6",
+    "url": "/ExBook/static/media/book5.ecceada9.jpg"
+  },
+  {
+    "revision": "0707f954d2e40601228b5a0f3167a46d",
+    "url": "/ExBook/static/media/book6.0707f954.jpg"
+  }
+]);
